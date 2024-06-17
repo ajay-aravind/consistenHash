8fbfd498556efc42f8f23b4d041dfd3209d6fa37
ajay-aravind <ajaynaredla348@gmail.com>
2024-06-17 11:05:05 +0530

    consistent hasing

---
+++ Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsistentHash
+{
+    //todo
+    // Add mroe hash functions
+    public class Server
+    {
+        internal String Id
+        {
+            get;
+        }
+        int VirtualServerPerServer;
+        internal List<String> PhysicalServerToVirtualServersMap
+        {
+            get;
+        }
+
+        public Server(string id, int virtualServerPerServer)
+        {
+            Id = id;
+            VirtualServerPerServer = virtualServerPerServer;
+            PhysicalServerToVirtualServersMap = new List<String>();
+        }
+
+
+
+        internal void GenerateVirtualServers()
+        {
+            for(int i=0;i< VirtualServerPerServer; i++)
+            {
+                string virtualServerId = Guid.NewGuid().ToString();
+                PhysicalServerToVirtualServersMap.Add(virtualServerId);
+            }
+            Console.WriteLine("Server: " + Id);
+            // print virtual servers
+            foreach (string virtualServer in PhysicalServerToVirtualServersMap)
+            {
+                Console.WriteLine("Virtual Server: " + virtualServer);
+            }
+        }
+
+
+        // Bernstein hash
+
+        int GetDjb2Hash(string s)
+        {
+            int hash = 5381;
+            foreach (char c in s)
+            {
+                    hash = (hash * 33) + c;
+            }
+            return hash;
+        }
+    }
+}
