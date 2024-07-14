5e44ef09dc19b9a4e41b1405a34ff7b0b2b9cec5
ajay-aravind <ajaynaredla348@gmail.com>
2024-07-14 22:43:26 +0530

    Refactored code and extracted methods for better readability

--- ConsistentHash/Ring.cs
+++ ConsistentHash/Ring.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsistentHash
 {
@@ -15,62 +17,31 @@ namespace ConsistentHash
         const int VirtualServerPerServer = 5;
         public Dictionary<String, int> requestCountPerServer = new Dictionary<string, int>();
 
+
+        public Ring()
+        {
+            this.AddNdummyServers(ringLength);
+        }
+
+        public Ring(int ringLength)
+        {
+            this.ringLength = ringLength;
+            this.AddNdummyServers(ringLength);
+        }
+
         // todo
         // Handler cases where there is a server already at generated index
-        internal Boolean AddServer(string id)
+        internal Boolean AddServer(string serverId)
         {
             try
             {
                 //print server id
-                Console.WriteLine("Server: " + id);
-                Server server = new Server(id, VirtualServerPerServer);
+                Console.WriteLine("Server: " + serverId);
+                Server server = new Server(serverId, VirtualServerPerServer);
                 servers.Add(server);
                 server.GenerateVirtualServers();
-                PhysicalServerToVirtualServersMap.Add(server.Id, server.PhysicalServerToVirtualServersMap);
-
-                // Add dummy 1000 virtual servers
-                for (int j = 0; j < ringLength; j++)
-                {
-                    virtualServers.AddLast(KeyValuePair.Create("", ""));
-                }
-
-                int i = 0;
-
-                foreach (string vServer in server.PhysicalServerToVirtualServersMap)
-                {
-                    uint hash = sdbmHash(vServer);
-                    //print hash
-                    Console.WriteLine("Hash: " + hash);
-                    int index = (int)(hash % ringLength);
-                    Console.WriteLine("Hash index: " + index);
-
-                    if (index < virtualServers.Count)
-                    {
-                        LinkedListNode<KeyValuePair<String, String>> linkedListNode = GetNodeAtIndex(index, virtualServers);
-                        //print linkedListNode
-                        Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
-                        var newServerNode = KeyValuePair.Create(server.PhysicalServerToVirtualServersMap[i], server.Id);
-                        virtualServers.AddBefore(linkedListNode, newServerNode);
-                        virtualServers.Remove(linkedListNode);
-                    }
-                    else
-                    {
-                        throw new Exception("Index out of bounds");
-                    }
-
-                    int k = 0;
-                    // print all virtual servers if key is not empty
-                    foreach (KeyValuePair<String, String> virtualServer in virtualServers)
-                    {
-                        if (virtualServer.Key != "")
-                        {
-                            Console.WriteLine("index:"+ k + "   Virtual Server: " + virtualServer.Key + " Server: " + virtualServer.Value);
-                        }
-                        k++;
-                    }
-
-                    i++;
-                }
+                this.PhysicalServerToVirtualServersMap.Add(server.Id, server.PhysicalServerToVirtualServersMap);
+                this.AddAllVirtualServersOfServerToRing(server);
             }
             catch (Exception e)
             {
@@ -81,52 +52,21 @@ namespace ConsistentHash
             return true;
         }
 
-        internal Boolean RemoveServer(string id)
+        internal Boolean RemoveServer(string serverId)
         {
             try
             {
-                List<String> virtualServers = PhysicalServerToVirtualServersMap[id];
+                List<String> virtualServers = PhysicalServerToVirtualServersMap[serverId];
                 foreach (string virtualServer in virtualServers)
                 {
-                    uint hash = sdbmHash(virtualServer);
-                    //print hash
-                    Console.WriteLine("Hash: " + hash);
-                    int index = (int)(hash % ringLength);
-                    Console.WriteLine("Hash index: " + index);
-                    LinkedListNode<KeyValuePair<String, String>> linkedListNode = GetNodeAtIndex(index, this.virtualServers);
-                    //print linkedListNode
-                    Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
-                    if (linkedListNode.Value.Key == virtualServer)
-                    {
-                        //print linkedListNode
-                        Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
-                        this.virtualServers.AddBefore(linkedListNode, KeyValuePair.Create("", ""));
-                        this.virtualServers.Remove(linkedListNode);
-                    }
-                }
-                PhysicalServerToVirtualServersMap.Remove(id);
-                this.servers.RemoveAll(server => server.Id == id);
-
-                // print physical server to virtual servers map
-                foreach (KeyValuePair<String, List<String>> entry in PhysicalServerToVirtualServersMap)
-                {
-                    Console.WriteLine("Server: " + entry.Key);
-                    foreach (string virtualServer in entry.Value)
-                    {
-                        Console.WriteLine("Virtual Server: " + virtualServer);
-                    }
+                    this.removeVirtualServer(virtualServer);
                 }
 
-                // print all virtual servers if key is not empty
-                int k = 0;
-                foreach (KeyValuePair<String, String> virtualServer in this.virtualServers)
-                {
-                    if (virtualServer.Key != "")
-                    {
-                        Console.WriteLine("index:" + k + "   Virtual Server: " + virtualServer.Key + " Server: " + virtualServer.Value);
-                    }
-                    k++;
-                }
+                PhysicalServerToVirtualServersMap.Remove(serverId);
+                this.servers.RemoveAll(server => server.Id == serverId);
+
+                this.PrintPhysicalServerToVirtualServersMap();
+                this.PrintNonEmptyVirtualServers();
             }
             catch (Exception e)
             {
@@ -135,13 +75,44 @@ namespace ConsistentHash
             }
             return false;
         }
+
+        private void removeVirtualServer(string virtualServer)
+        {
+            uint hash = sdbmHash(virtualServer);
+            //print hash
+            Console.WriteLine("Hash: " + hash);
+            int index = (int)(hash % ringLength);
+            Console.WriteLine("Hash index: " + index);
+            LinkedListNode<KeyValuePair<String, String>> linkedListNode = GetNodeAtIndex(index, this.virtualServers);
+            //print linkedListNode
+            Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
+            if (linkedListNode.Value.Key == virtualServer)
+            {
+                //print linkedListNode
+                Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
+                this.virtualServers.AddBefore(linkedListNode, KeyValuePair.Create("", ""));
+                this.virtualServers.Remove(linkedListNode);
+            }
+        }
+
+        private void PrintPhysicalServerToVirtualServersMap()
+        {
+            // print physical server to virtual servers map
+            foreach (KeyValuePair<String, List<String>> entry in this.PhysicalServerToVirtualServersMap)
+            {
+                Console.WriteLine("Server: " + entry.Key);
+                foreach (string virtualServer in entry.Value)
+                {
+                    Console.WriteLine("Virtual Server: " + virtualServer);
+                }
+            }
+        }
+
         internal String GetServerToHandleRequest(string requestId)
         {
-            
             // print request id
             //Console.WriteLine("Request: " + requestId);
             uint hash = sdbmHash(requestId);
-            
             int index = (int)(hash % ringLength);
             //Console.WriteLine("Hash index: " + index);
             var node = GetNodeAtOrAfterIndex(index, virtualServers);
@@ -149,16 +120,66 @@ namespace ConsistentHash
             //Console.WriteLine("Node: " + node.Value.Key);
             //Console.WriteLine("Node: " + node.Value.Value);
             // add or increment request count per server
-            if (requestCountPerServer.ContainsKey(node.Value.Value))
+            this.UpdateRequestCountPerServer(node.Value.Value);
+            return node.Value.Key;
+        }
+
+        private void AddVirtualServerToRingAtIndex(int index, Server server, String virtualServerId)
+        {
+            LinkedListNode<KeyValuePair<String, String>> linkedListNode = GetNodeAtIndex(index, virtualServers);
+            //print linkedListNode
+            Console.WriteLine("LinkedListNode: " + linkedListNode.Value);
+            var newServerNode = KeyValuePair.Create(virtualServerId, server.Id);
+            virtualServers.AddBefore(linkedListNode, newServerNode);
+            virtualServers.Remove(linkedListNode);
+        }
+
+        private void PrintNonEmptyVirtualServers()
+        {
+            int k = 0;
+            // print all virtual servers if key is not empty
+            foreach (KeyValuePair<String, String> virtualServer in this.virtualServers)
             {
-                requestCountPerServer[node.Value.Value] = requestCountPerServer[node.Value.Value] + 1;
+                if (virtualServer.Key != "")
+                {
+                    Console.WriteLine("index:" + k + "   Virtual Server: " + virtualServer.Key + " Server: " + virtualServer.Value);
+                }
+                k++;
+            }
+        }
+
+        private void AddAllVirtualServersOfServerToRing(Server server)
+        {
+            foreach (string vServer in server.PhysicalServerToVirtualServersMap)
+            {
+                uint hash = sdbmHash(vServer);
+                //print hash
+                Console.WriteLine("Hash: " + hash);
+                int index = (int)(hash % ringLength);
+                Console.WriteLine("Hash index: " + index);
+
+                if (index < this.virtualServers.Count)
+                {
+                    this.AddVirtualServerToRingAtIndex(index, server, vServer);
+                }
+                else
+                {
+                    throw new Exception("Index out of bounds");
+                }
+                this.PrintNonEmptyVirtualServers();
+            }
+        }
+
+        private void UpdateRequestCountPerServer(String serverId)
+        {
+            if (requestCountPerServer.ContainsKey(serverId))
+            {
+                requestCountPerServer[serverId] = requestCountPerServer[serverId] + 1;
             }
             else
             {
-                requestCountPerServer.Add(node.Value.Value, 1);
+                requestCountPerServer.Add(serverId, 1);
             }
-
-            return node.Value.Key;
         }
 
         private LinkedListNode<KeyValuePair<String, String>> GetNodeAtOrAfterIndex(int index, LinkedList<KeyValuePair<string, string>> virtualServers)
@@ -167,16 +188,26 @@ namespace ConsistentHash
             //print node
             //Console.WriteLine("Node: " + node.Value.Key);
 
-            while (node != null)
+             node = GetNextValidNode(node, index, virtualServers);
+            if(node != null)
             {
-                if (node.Value.Key != "" && index <= virtualServers.Count)
+                return node;
+            }
+            else
+            {
+                node = this.GetValidNodeFromStartToIndex(index, virtualServers);
+                if (node != null)
                 {
                     return node;
                 }
-                node = node.Next;
             }
 
-            node = virtualServers.First;
+            return null;
+        }
+
+        private LinkedListNode<KeyValuePair<String, String>>? GetValidNodeFromStartToIndex(int index, LinkedList<KeyValuePair<String, String>> virtualServers)
+        {
+            var node = virtualServers.First;
             //print node
             //Console.WriteLine("Node: " + node.Value.Key);
             int currentIndex = 0;
@@ -190,12 +221,30 @@ namespace ConsistentHash
                 node = node.Next;
                 currentIndex++;
             }
+            return null;
+        }
+
+        private LinkedListNode<KeyValuePair<String, String>>? GetNextValidNode(LinkedListNode<KeyValuePair<String, String>> node, int index, LinkedList<KeyValuePair<String, String>> virtualServers)
+        {
+            while (node != null)
+            {
+                if (node.Value.Key != "" && index <= virtualServers.Count)
+                {
+                    return node;
+                }
+                node = node.Next;
+            }
 
             return null;
         }
 
-        internal LinkedListNode<KeyValuePair<String, String>> GetNodeAtIndex(int index, LinkedList<KeyValuePair<String, String>> virtualServers)
-        {
+        private LinkedListNode<KeyValuePair<String, String>> GetNodeAtIndex(int index, LinkedList<KeyValuePair<String, String>> virtualServers)
+        { 
+            if(virtualServers.Count == 0)
+            {
+                throw new Exception("Virtual servers list is empty");
+            }
+
             LinkedListNode<KeyValuePair<String,String>> node = virtualServers.First;
             for (int i = 0; i < index; i++)
             {
@@ -204,6 +253,16 @@ namespace ConsistentHash
             return node;
         }
 
+        private void AddNdummyServers(int ringLength)
+        {
+            // Add dummy 1000 virtual servers
+            for (int j = 0; j < ringLength; j++)
+            {
+                this.virtualServers.AddLast(KeyValuePair.Create("", ""));
+            }
+        }
+
+        /*
         private IEnumerable<uint> GenerateIndexesForVirtualServers(List<string> virtualServers, int ringLength)
         {
             HashSet<uint> indexes = new HashSet<uint>();
@@ -254,6 +313,7 @@ namespace ConsistentHash
 
             return hash;
         }
+        */
 
         // SDBM Hash
         private uint sdbmHash(string str)
